f54d186f38813f201aa3018d69697cb77c1186e8
chuka <g00414637@atu.ie>
2024-10-14 22:39:08 +0100

    finishing work at home

---
+++ src/main/java/ie/atu/week3cicd/productController.java
@@ -0,0 +1,48 @@
+package ie.atu.week3cicd;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class productController {
+    private List<product> productList = new ArrayList<>();
+
+    public productController(){
+        productList.add(new product("1","TV","Electronics",600.0));
+        productList.add(new product("2","Phone","Electronics",500.0));
+    }
+
+    @GetMapping
+    public List<product> getAllProducts(){
+        return productList;
+    }
+
+    @PutMapping("/{id}")
+    public product updateProduct(@PathVariable("id") String id, @RequestBody product updatedProduct) {
+        for (product prod : productList) {
+            if (prod.getId().equals(id)) {
+                prod.setName(updatedProduct.getName());
+                prod.setCategory(updatedProduct.getCategory());
+                prod.setPrice(updatedProduct.getPrice());
+                return prod;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable("id") String id) {
+        for (product p : productList) {
+            if (p.getId().equals(id)) {
+                productList.remove(p);
+                return "Product with ID " + id + " has been deleted.";
+            }
+        }
+        return id + " not found.";
+    }
+
+
+}
